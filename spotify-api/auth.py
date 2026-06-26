ea54b39bff27a6f7a15482b41e789a51101cb78f
grekszadalma <grekszadalma@gmail.com>
2026-06-26 15:28:07 +0200

    Add comments to Spotify Web API

--- spotify-api/auth.py
+++ spotify-api/auth.py
@@ -12,9 +12,20 @@ REDIRECT_URI = os.getenv("REDIRECT_URI")
 AUTH_URL = "https://accounts.spotify.com/authorize"
 TOKEN_URL = "https://accounts.spotify.com/api/token"
 
-
+# authentification
 def get_auth_url():
+
+    # this tells spotify what we want authorization for
     scopes = "user-top-read user-read-recently-played"
+
+
+
+    ### Creates a URL string ###
+    # AUTH_URL: constant, beginning of the url (uses the authorize API of Spotify) 
+    # Response type: After the user logs in, send me an authorization code
+    # Client ID: This identifies which Spotify application is making the request (Which app we created in Spotify Developer Dashboard)
+    # Scope: What we want permission to access
+    # Redirect URI: After the user logs in, send them back here.
 
     return (
         f"{AUTH_URL}"
@@ -24,7 +35,7 @@ def get_auth_url():
         f"&redirect_uri={REDIRECT_URI}"
     )
 
-
+# get the access token
 def get_token(code: str):
     auth_header = base64.b64encode(
         f"{CLIENT_ID}:{CLIENT_SECRET}".encode()
